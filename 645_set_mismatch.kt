04473c3629ec8b6c3f92eca7dd0820669bf3d69f
Nate Brown <nate@nateb.dev>
2024-01-22 09:32:15 +0800

    oops this is the working 645

--- 645_set_mismatch.kt
+++ 645_set_mismatch.kt
@@ -2,12 +2,21 @@ fun findErrorNums(nums: IntArray): IntArray {
     //Probably faster to use binary search so I need to practice that
     //Maybe implement that tonight
     //According to editorial, using a hashmap is an optimal solution lol
-
-    val sorted = nums.sorted();
-    for (i in sorted.indices) {
-        if (sorted[i] != i + 1) {
-            return intArrayOf(nums[i], i + 1)
+    val map = mutableMapOf<Int, Int>()
+    for (num in nums) {
+        map[num] = map.getOrDefault(num, 0) + 1;
+    }
+    var count = 1;
+    var missing = 0;
+    var dupe = 0;
+    for (i in 1..nums.size) {
+        if (map.containsKey(i)) {
+            if (map[i] == 2) {
+                dupe = i;
+            }
+        } else {
+            missing = i;
         }
     }
-    return nums;
+    return intArrayOf(dupe, missing);
 }
